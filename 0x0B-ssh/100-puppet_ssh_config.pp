634327fd6c0f5aa5d29eec64e1a14d48cef0915f
TatendaTorerwa <ttorerwa01@gmail.com>
2024-01-19 13:48:06 -0800

    4. Client configuration file (w/ Puppet).

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Using Puppet to make changes to our configuration file.
+
+file { '/etc/ssh/ssh_config':
+  ensure => 'present',
+content =>"
+        #SSH Client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	passwordAuthentication no"
+} 
