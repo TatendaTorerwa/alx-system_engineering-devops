95e8118d906d69a3f28ecfcf13baacb331981daa
TatendaTorerwa <ttorerwa01@gmail.com>
2024-03-05 10:15:19 -0800

    0. How many .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Linux:0x16.api.advanced:v1.1 (by /u/Tatenda)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 400:
         return 0
+    final_data = response.json().get("data")
+    return final_data.get("subscribers")
