68b265124940e0f5b579ebc16a9c7a4c278a91b1
TatendaTorerwa <ttorerwa01@gmail.com>
2024-03-05 10:56:01 -0800

    1. Top Ten.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Prints the titles of the first 10 hot posts listed for a given subreddit."""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Titles of the 10 hottest posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        if not posts:
+            print("No posts found for this subreddit.")
+        else:
+            for post in posts:
+                print(post['data']['title'])
+    else:
+        print("None")
