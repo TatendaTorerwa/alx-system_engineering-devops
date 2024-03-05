5f20c841577283d66b2c34728cf70277570a982d
TatendaTorerwa <ttorerwa01@gmail.com>
2024-03-05 10:22:01 -0800

    redo.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and
-returns the number of subscribers for a given subreddit.
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Linux:0x16.api.advanced:v1.1 (by /u/Tatenda)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 400:
+    get_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/mishakmosi)"
+    }
+    fin_res = requests.get(get_url, headers=headers, allow_redirects=False)
+    if fin_res.status_code == 404:
         return 0
-    final_data = response.json().get("data")
-    return final_data.get("subscribers")
+    results = fin_res.json().get("data")
+    return results.get("subscribers")
